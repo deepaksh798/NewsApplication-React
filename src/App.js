aa44896df61ecad0e940ef212cf302b3274cfcdb
hritiksharma <69796067+hritiksharma@users.noreply.github.com>
2024-02-07 17:40:54 +0000

    updated commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import News from "./components/News";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
+import NewHome from "./components/NewHome";
 
 const App = () => {
   const pageSize = 5;
   const country = "in";
-  const apiKey = process.env.REACT_APP_NEWS_API;
+  // const apiKey = process.env.REACT_APP_NEWS_API;
 
   const [progress, setProgress] = useState(0);
 
@@ -20,14 +21,15 @@ const App = () => {
         <LoadingBar height={3} color="#f11946" progress={progress} />
         <NavBar />
         <Routes>
-          <Route exact path="/" element={<News setProgress={setProgress} key="general" apiKey={apiKey} pageSize={pageSize} country={country} category="general" />} />
-          <Route exact path="/business" element={<News setProgress={setProgress} key="business" apiKey={apiKey} pageSize={pageSize} country={country} category="business" />} />
+          <Route exact path="/" element = { <NewHome />} /> 
+          {/* <Route exact path="/" element={<News setProgress={setProgress} key="general" apiKey={apiKey} pageSize={pageSize} country={country} category="general" />} /> */}
+          {/* <Route exact path="/business" element={<News setProgress={setProgress} key="business" apiKey={apiKey} pageSize={pageSize} country={country} category="business" />} />
           <Route exact path="/entertainment" element={<News setProgress={setProgress} key="entertainment" apiKey={apiKey} pageSize={5} country={country} category="entertainment" />} />
           <Route exact path="/general" element={<News setProgress={setProgress} key="general" apiKey={apiKey} pageSize={pageSize} country={country} category="general" />} />
           <Route exact path="/health" element={<News setProgress={setProgress} key="health" apiKey={apiKey} pageSize={pageSize} country={country} category="health" />} />
           <Route exact path="/science" element={<News setProgress={setProgress} key="science" apiKey={apiKey} pageSize={pageSize} country={country} category="science" />} />
           <Route exact path="/sports" element={<News setProgress={setProgress} key="sports" apiKey={apiKey} pageSize={pageSize} country={country} category="sports" />} />
-          <Route exact path="/technology" element={<News setProgress={setProgress} key="technology" apiKey={apiKey} pageSize={pageSize} country={country} category="technology" />} />
+          <Route exact path="/technology" element={<News setProgress={setProgress} key="technology" apiKey={apiKey} pageSize={pageSize} country={country} category="technology" />} /> */}
         </Routes>
       </Router>
     </div>
---
+++ src/components/NewHome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewHome = () => {
+    console.log("inside NewHome");
+  return (
+    <div>
+      <h1 style={{background:"blue", marginTop:"50px"}} > New Home </h1>
+    </div>
+  )
+}
+
+export default NewHome
